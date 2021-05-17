eb71998e17257b5e14bd550f7f6dc961d439921c
Leon <410763533@qq.com>
2021-05-17 15:41:16 +0800

    money页面布局

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,5 +1,6 @@
+import Nav from '@/components/Nav.vue';
 <template>
-  <div>
+  <div class="nav">
     <router-link to="/money">记账</router-link>
     |
     <router-link to="/labels">标签</router-link>
@@ -14,5 +15,8 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.nav {
+  border: 1px solid red;
+}
 </style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,30 @@
 <template>
-  <div>
-    Money.vue
+  <div class="nav-wrapper">
+    <div class="content">
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+    </div>
     <hr />
     <Nav />
   </div>
@@ -13,4 +37,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.nav-wrapper {
+  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+.content {
+  overflow: auto;
+  flex-grow: 1;
+}
 </style>
